81de9f163f475206fd45948d0b37af81435096f2
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2024-12-05 14:07:55 +0800

    refactor(day3): use `foldl'` to avoid recursion

--- day3.nix
+++ day3.nix
@@ -1,44 +1,57 @@
 { input, lib }:
 let
-  inherit (builtins) substring stringLength;
+  inherit (builtins)
+    substring
+    stringLength
+    foldl'
+    isInt
+    ;
+
+  initialState = {
+    idx = 0;
+    type = "idle";
+    result = 0;
+  };
 
   processChar =
     readToggles:
-    lib.fix (
-      self: state:
+    foldl' (
+      state: _:
       let
         char = substring state.idx 1 input;
       in
-      if state.idx >= (stringLength input) then
-        state.result # we're done here
+      if isInt state then
+        state
+      else if state.idx >= (stringLength input) then
+        state.result
       else if state.type == "idle" then
         if substring state.idx 4 input == "mul(" then
-          self {
+          {
             inherit (state) result;
             type = "fst";
             fst = 0;
             idx = state.idx + 4;
           }
         else if readToggles && substring state.idx 7 input == "don't()" then
-          self {
+          {
             inherit (state) result;
             type = "off";
             idx = state.idx + 7;
           }
         else
-          self {
+          {
             inherit (state) type result;
             idx = state.idx + 1;
           }
       else if readToggles && state.type == "off" then
         if substring state.idx 4 input == "do()" then
-          self {
+          {
             inherit (state) result;
             type = "idle";
             idx = state.idx + 4;
           }
         else
-          self {
+          {
             inherit (state) type result;
             idx = state.idx + 1;
           }
@@ -47,20 +60,20 @@ let
           digit = lib.aoc.safeDigitToInt char;
         in
         if digit != null then
-          self {
+          {
             inherit (state) type result;
             fst = state.fst * 10 + digit;
             idx = state.idx + 1;
           }
         else if char == "," then
-          self {
+          {
             inherit (state) result fst;
             type = "snd";
             snd = 0;
             idx = state.idx + 1;
           }
         else
-          self {
+          {
             inherit (state) result;
             type = "idle";
             idx = state.idx + 1;
@@ -70,34 +83,28 @@ let
           digit = lib.aoc.safeDigitToInt char;
         in
         if digit != null then
-          self {
+          {
             inherit (state) type fst result;
             snd = state.snd * 10 + digit;
             idx = state.idx + 1;
           }
         else if char == ")" then
-          self {
+          {
             type = "idle";
             result = state.result + state.fst * state.snd;
             idx = state.idx + 1;
           }
         else
-          self {
+          {
             inherit (state) result;
             type = "idle";
             idx = state.idx + 1;
           }
       else
         throw "unknown state: ${toString state}"
-    );
-
-  initialState = {
-    idx = 0;
-    type = "idle";
-    result = 0;
-  };
+    ) initialState (lib.replicate (stringLength input) null);
 in
 {
-  part1 = processChar false initialState;
-  part2 = processChar true initialState;
+  part1 = processChar false;
+  part2 = processChar true;
 }
